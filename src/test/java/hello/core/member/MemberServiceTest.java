9809361ce76a9d8237bccdb8217576bbbdad2d0f
MS269 <ehddnr9797@gmail.com>
2023-06-29 23:45:54 -0400

    only java using AppConfig

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    private final MemberService memberService = new MemberServiceImpl();
+    private MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,23 +1,28 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
 
-    private final MemberService memberService = new MemberServiceImpl();
-    private final OrderService orderService = new OrderServiceImpl();
+    private MemberService memberService;
+    private OrderService orderService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
-
         Long memberId = 1L;
         Member newMember = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(newMember);
